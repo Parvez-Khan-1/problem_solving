808275d3fedf2ede1bf567aa52454411626b270e
Parvez-Khan-1 <parvezpathan09@gmail.com>
2021-04-10 12:58:51 +0530

    EasyLevel: Valid Parenthesis

---
+++ leetcode_easy/valid_parenthesis.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/valid-parentheses/
+# Valid Assumption: There wont be any chars/nums except (, {, [, ], }, )
+
+
+class Solution:
+    def isValid(self, s: str):
+        stop = True
+        original_length = len(s)
+        while stop:
+            s = s.replace("{}", "")
+            s = s.replace("[]", "")
+            s = s.replace("()", "")
+            if len(s) < original_length:
+                original_length = len(s)
+            else:
+                stop = False
+        if len(s) > 0:
+            return False
+        else:
+            return True
+
+exp = "{{}[][[]]]}"
+
+s = Solution()
+print(s.isValid(exp))
