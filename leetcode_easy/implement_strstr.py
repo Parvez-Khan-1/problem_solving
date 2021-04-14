b65727c8db7250b8221ff8e683cc47d02be18632
unknown <parvezpathan09@gmail.com>
2021-04-14 23:04:17 +0530

    implement strstr

---
+++ leetcode_easy/implement_strstr.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/implement-strstr/
+
+
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if len(haystack) >0:
+#             return haystack.find(needle)
+#         else:
+#             if len(needle)>0:
+#                 return -1
+#             return 0
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) >0:
+            for
+
+        else:
+            if len(needle)>0:
+                return -1
+            return 0
+
+
+s = Solution()
+print(s.strStr(haystack="hello", needle="ll"))
